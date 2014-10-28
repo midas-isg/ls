7620e3d8f94015861f04403cac2077c9f3a84bd3
Amin <amin.tajgardoon@gmail.com>
2014-10-28 10:13:39 -0400

    added two tables "administrative_unit" and "audit_administrative_unit" hibernate auto-creates/auto-update tables

---
+++ app/dao/entities/AuditAdministrativeUnit.java
@@ -0,0 +1,114 @@
+package dao.entities;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
+import play.db.jpa.JPA;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+@Entity
+@Table(name = "audit_administrative_unit")
+public class AuditAdministrativeUnit {
+
+	@Id
+	@Column(name = "id", columnDefinition = "serial")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	@Column(name = "start_date")
+	private Date startDate;
+
+	@Column(name = "end_date")
+	private Date endDate;
+
+	@Type(type = "org.hibernate.spatial.GeometryType")
+	private Geometry geom;
+
+	private boolean locked;
+
+	@Column(name = "update_date")
+	private Date updateDate;
+
+	@Column(name = "user_id")
+	private String userId;
+
+	public static AuditAdministrativeUnit findById(Long id) {
+		return JPA.em().find(AuditAdministrativeUnit.class, id);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Geometry getGeom() {
+		return geom;
+	}
+
+	public void setGeom(Geometry geom) {
+		this.geom = geom;
+	}
+
+	public boolean isPretected() {
+		return locked;
+	}
+
+	public void setPretected(boolean pretected) {
+		this.locked = pretected;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public String getUser() {
+		return userId;
+	}
+
+	public void setUser(String user) {
+		this.userId = user;
+	}
+
+}
