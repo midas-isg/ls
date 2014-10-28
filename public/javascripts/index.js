b43fb18758a11674426cac7c6a97806abba6104d
TPS23 <tps23@pitt.edu>
2014-10-28 15:47:38 -0400

    Fixed loading/saving

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -15,12 +15,13 @@ $(document).ready(function() {
 function MapDriver(){
 	this.title = '<strong>Pitt</strong>sburgh';
 	this.mapID = 'tps23.k1765f0g';
-	this.geojsonFile = 'http://tps23-nb.univ.pitt.edu/pittsburgh.json'; //'http://localhost:9000/leaflet/nyc'; //'http://localhost/countries.geo.json';
+	this.geojsonFile = 'http://tps23-nb.univ.pitt.edu/test.json'; //'http://localhost:9000/leaflet/nyc'; //'http://localhost/countries.geo.json';
 	this.featureLayerObject = null;
 	this.startingCoordinates = [40.45826240784896, -79.93491411209106]; //[44.95167427365481, 582771.4257198056];
 	this.zoom = 12;
 	this.accessToken = 'pk.eyJ1IjoidHBzMjMiLCJhIjoiVHEzc0tVWSJ9.0oYZqcggp29zNZlCcb2esA';
 	this.featureLayer = null;
+	this.map = null;
 	
 	this.initialize();
 	
@@ -30,41 +31,65 @@ function MapDriver(){
 MapDriver.prototype.initialize = function() {
 	L.mapbox.accessToken = this.accessToken;
 	
-	var map = L.mapbox.map('map-one', 'examples.map-i86l3621', { worldCopyJump: true /*crs: L.CRS.EPSG385*/}).setView(this.startingCoordinates, this.zoom);
-	map.legendControl.addLegend(this.title);
-	
-	this.featureLayer = L.mapbox.featureLayer().addTo(map);
+	this.map = L.mapbox.map('map-one', 'examples.map-i86l3621', { worldCopyJump: true /*crs: L.CRS.EPSG385*/}).setView(this.startingCoordinates, this.zoom);
+	this.map.legendControl.addLegend(this.title);
 	
 	if(this.geojsonFile) {
-		this.featureLayerObject = this.featureLayer.loadURL(this.geojsonFile);
+		this.featureLayer = L.mapbox.featureLayer().loadURL(this.geojsonFile);
+	}
+	else if(this.mapID) {
+		this.featureLayer = L.mapbox.featureLayer().loadID(this.mapID);
 	}
 	
-	if(!this.featureLayerObject && this.mapID) {
-		console.log("couldn't load json; attempting to load via map ID");
-		this.featureLayer.loadID(this.mapID);
-	}
+	this.featureLayer.on('ready', function() {
+		MAP_DRIVER.featureLayer.addTo(MAP_DRIVER.map);
+		
+		var drawControl = new L.Control.Draw({
+			edit: {
+			  featureGroup: MAP_DRIVER.featureLayer
+			}
+		}).addTo(MAP_DRIVER.map);
+		
+		MAP_DRIVER.map.on('draw:created', function(e) {
+			MAP_DRIVER.featureLayer.addLayer(e.layer);
+		});
+	});
 	
-	var drawControl = new L.Control.Draw({
-		edit: {
-		  featureGroup: this.featureLayer
+	this.featureLayer.on('error', function(err) {
+		console.log("Error: " + err['error']['statusText']);
+		
+		if((MAP_DRIVER.featureLayer.getLayers().length == 0) && MAP_DRIVER.mapID) {
+			console.log("Attempting to load via mapID");
+			MAP_DRIVER.featureLayer = L.mapbox.featureLayer().loadID(MAP_DRIVER.mapID);
 		}
-	}).addTo(map);
-	
-	map.on('draw:created', function(e) {
-		MAP_DRIVER.featureLayer.addLayer(e.layer);
+		
+		MAP_DRIVER.featureLayer.on('ready', function() {
+			MAP_DRIVER.featureLayer.addTo(MAP_DRIVER.map);
+			
+			var drawControl = new L.Control.Draw({
+				edit: {
+				  featureGroup: MAP_DRIVER.featureLayer
+				}
+			}).addTo(MAP_DRIVER.map);
+			
+			MAP_DRIVER.map.on('draw:created', function(e) {
+				MAP_DRIVER.featureLayer.addLayer(e.layer);
+			});
+		});
 	});
 	
 	return;
 }
 
 MapDriver.prototype.download = function() {
-	var jsonData = this.featureLayer.getGeoJSON();
+	var jsonData = this.featureLayer.toGeoJSON();
+	jsonData['id'] = this.mapID;
 	
 	var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(jsonData));
 	
 	//$('<a href="data:' + data + '" download="data.json">download JSON</a>').appendTo('#container');
 	
-	location.assign("data:'" + data + "'");
+	location.assign("data:'" + data);
 	
 	return jsonData;
 }
