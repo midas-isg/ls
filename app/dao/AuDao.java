6a1e1fd56166e2e4f48b4c1414a0c77022e39b1f
saw111 <saw111@pitt.edu>
2014-11-24 15:56:26 -0500

    Supported all CRUD in DAO.

--- app/dao/AuDao.java
+++ app/dao/AuDao.java
@@ -8,18 +8,33 @@ import dao.entities.AdministrativeUnit;
 
 public class AuDao {
 
-	public Long save(AdministrativeUnit au) {
+	public Long create(AdministrativeUnit au) {
 		EntityManager em = JPA.em();
-		em.merge(au);
+		em.persist(au);
 		Long gid = au.getGid();
-		Logger.debug("Saved " + gid);
+		Logger.debug("persisted " + gid);
 		return gid;
 	}
 
-	public AdministrativeUnit findByGid(long gid) {
+	public AdministrativeUnit read(long gid) {
 		EntityManager em = JPA.em();
 		AdministrativeUnit result = em.find(AdministrativeUnit.class, gid);
 		return result;
 	}
+	
+	public Long update(AdministrativeUnit au) {
+		EntityManager em = JPA.em();
+		em.merge(au);
+		Long gid = au.getGid();
+		Logger.debug("merged " + gid);
+		return gid;
+	}
 
+	public Long delete(AdministrativeUnit au) {
+		EntityManager em = JPA.em();
+		Long gid = au.getGid();
+		em.remove(au);
+		Logger.debug("removed " + gid);
+		return gid;
+	}
 }
