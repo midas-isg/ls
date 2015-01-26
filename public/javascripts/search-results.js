f7b305a3b9f31a1cd37672523bb99933517d17f2
TPS23 <tps23@pitt.edu>
2015-01-26 16:43:18 -0500

    decoupled searchbar suggestion functionality from search results functionality

--- public/javascripts/search-results.js
+++ public/javascripts/search-results.js
@@ -7,25 +7,6 @@ $(document).ready(function() {
 			return;
 	});
 	
-	$("#input").keyup(function(event) {
-		switch(event.which)
-		{
-			case 13:
-				$("#search-button").click();
-				$("#search-button").focus();
-			break;
-			
-			default:
-			break;
-		}
-		
-		return;
-	});
-	
-	var limit = 5;
-	
-	bindSuggestionBox("#input", context + "/api/unique-location-names?limit=" + limit + "&q=");
-	
 	var query = getURLParameterByName("q");
 	
 	if(query) {
@@ -57,6 +38,7 @@ function searchClick() {
 		}
 	});
 	
+	return;
 }
 
 var geoJSON;
@@ -114,31 +96,6 @@ function updateOutput(data, status, result) {
 			}
 		}
 	}
-	
-	return;
-}
-
-function bindSuggestionBox(inputBox, URL) {
-	jQuery(inputBox).autocomplete({
-		source: function (request, response) {
-			jQuery.get(URL + $(inputBox).val(), {
-				name: request.name
-			}, function (data) {
-				var index = 0;
-				var locations = [];
-				
-				locations[0] = "No search results found...";
-				
-				for(index = 0; index < data.length; index++) {
-					locations[index] = data[index]['name'];
-				}
-				
-				response(locations);
-			});
-		},
-		minLength: 3,
-		delay: 500
-	});
 	
 	return;
 }
