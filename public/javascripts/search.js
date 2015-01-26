dfa9ba298860c72bbc0979b0adf3692741d9295a
TPS23 <tps23@pitt.edu>
2015-01-26 13:39:43 -0500

    fixed line-height of search bar div and relocated search bar suggestion binding function to search script

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 		{
 			case 13:
 				$("#search-button").click();
+				$("#search-button").focus();
 			break;
 			
 			default:
@@ -100,6 +101,31 @@ function updateOutput(data, status, result) {
 			resultBody.append(appendString);
 		}
 	}
+	
+	return;
+}
+
+function bindSuggestionBox(inputBox, URL) {
+	jQuery(inputBox).autocomplete({
+		source: function (request, response) {
+			jQuery.get(URL + $(inputBox).val(), {
+				name: request.name
+			}, function (data) {
+				var index = 0;
+				var locations = [];
+				
+				locations[0] = "No search results found...";
+				
+				for(index = 0; index < data.length; index++) {
+					locations[index] = data[index]['name'];
+				}
+				
+				response(locations);
+			});
+		},
+		minLength: 3,
+		delay: 500
+	});
 	
 	return;
 }
