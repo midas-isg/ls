95b930cf0e9bb7af78a102571de5f673246a2885
saw111 <saw111@pitt.edu>
2015-02-04 16:24:30 -0500

    Rename table location_optimized_geometry to location_low_resolution_geometry.

--- app/dao/entities/LocationLowResolutionGeometry.java
+++ app/dao/entities/LocationLowResolutionGeometry.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="location_optimized_geometry")
+@Table(name="location_low_resolution_geometry")
 public class LocationLowResolutionGeometry extends LocationGeometry {
 
 }
