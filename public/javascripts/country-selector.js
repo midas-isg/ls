86bd455d6268b874c9ead668ab5a3fe38ae854ef
TPS23 <tps23@pitt.edu>
2018-03-05 18:07:29 -0500

    Implemented 'Get All Countries' button

--- public/javascripts/country-selector.js
+++ public/javascripts/country-selector.js
@@ -1,23 +1,23 @@
+"use strict";
+
 (function(HELPERS) {
 	$(document).ready(function() {
-		var countriesURL = CONTEXT + "/api/locations/find-by-type/1?_v=2&_onlyFeatureFields=properties.name%2Cproperties.gid&limit=0";
+		var countriesURL = CONTEXT + "/api/locations/find-by-type/1?_v=2&_onlyFeatureFields=properties.name%2Cproperties.gid&limit=0",
+			countries = [];
 		
 		$.get(countriesURL, function(data, status, xhr) {
 			var countryInput = document.getElementById("country"),
 				countryOption,
-				tempArray = [],
 				i;
 			
 			for(i = 0; i < data.features.length; i++) {
-				tempArray.push({
+				countries.push({
 					name: data.features[i].properties.name,
 					gid: data.features[i].properties.gid
 				});
 			}
 			
-			tempArray.sort(HELPERS.nameCompare);
-			
-			countries = tempArray;
+			countries.sort(HELPERS.nameCompare);
 			
 			for(i = 0; i < countries.length; i++) {
 				countryOption = document.createElement("option");
@@ -25,6 +25,21 @@
 				countryOption.innerHTML = countries[i].name;
 				countryInput.appendChild(countryOption);
 			}
+			
+			return;
+		});
+		
+		$("#get-all-countries").click(function() {
+			var countriesIDs = countries[0].gid,
+				i;
+			
+			for(i = 1; i < countries.length; i++) {
+				countriesIDs += ", " + countries[i].gid;
+			}
+			
+			$("#location-codes-list").text(countriesIDs);
+			$("#code-type").val(14);
+			alert("This query may take a few minutes! Click Submit when ready.");
 			
 			return;
 		});
